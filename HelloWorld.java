d95fc1ec0ba0af75b8c9b9b296ce82f9a8ca9428
rss74 <78577219+rss74@users.noreply.github.com>
2021-07-22 12:32:04 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, Universe");
+      System.out.println("Hello, World");
       System.out.println("Hello, Solar");
       System.out.println("Hello, Earth");
    }
